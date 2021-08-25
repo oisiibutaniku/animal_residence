950901cac188b5bb98a70e7bfe5497cd5ace4c51
oisiibutaniku <butanoosasimi@gmail.com>
2021-08-25 15:58:17 +0900

    定期５

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,6 +2,8 @@ class Tag < ApplicationRecord
   has_many :animal_tag_relations
   has_many :animals, through: :animal_tag_relations
 
+  validates :name, uniqueness: true
+
   def self.search(search)
     if search != '#'
       tag = Tag.where(name: search)
